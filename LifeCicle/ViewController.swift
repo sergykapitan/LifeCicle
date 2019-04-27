bd42a755c948f892346e3978643f93f1f5065273
Sergey <12kaptan12@gmail.com>
2019-04-27 14:57:51 +0300

    Initial Commit

---
+++ LifeCicle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCicle
+//
+//  Created by Sergey Koriukin on 27/04/2019.
+//  Copyright © 2019 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
